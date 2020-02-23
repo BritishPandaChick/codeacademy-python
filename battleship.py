c003fadd1b97558670ef1278c41d58667decb111
BritishPandaChick <sbartley1080@gmail.com>
2020-02-23 15:09:53 -0500

    Add functions random_row and random_col

--- battleship.py
+++ battleship.py
@@ -7,4 +7,11 @@ def print_board(board_in):
     for row in board_in:
         print " ".join(row)
 
-print_board(board)
+def random_row(board_in):
+    return randint(0, len(board_in) - 1)
+
+def random_col(board_in):
+    return randint(0, len(board_in) - 1)
+
+random_row(board)
+random_col(board)
